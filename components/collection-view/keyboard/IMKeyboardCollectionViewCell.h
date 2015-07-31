28794438b62ef00dd03426601ef4ef3075ef1118
nkhoshini <nima@imojiapp.com>
2015-07-30 19:44:28 -0700

    migrate out keyboard specific collection view components to subclasses of the base collection view classes

---
+++ components/collection-view/keyboard/IMKeyboardCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+// Created by Nima on 7/30/15.
+// Copyright (c) 2015 Jeff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IMCollectionViewCell.h"
+
+
+@interface IMKeyboardCollectionViewCell : IMCollectionViewCell
+
+- (void)performAnimation;
+
+@end
