300641155e9381e5476d74c3b89aae269ddddaa0
nkhoshini <nima@imojiapp.com>
2015-08-31 16:00:00 -0700

    moved all keyboard files and resources to shared pod repo

---
+++ Examples/Keyboard/Source/View Controllers/IMSampleKeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  IMSampleKeyboardViewController.h
+//  imoji-keyboard
+//
+//  Created by Nima on 8/31/15.
+//  Copyright © 2015 Jeff. All rights reserved.
+//
+
+#import <ImojiSDKUI/IMKeyboardViewController.h>
+
+@interface IMSampleKeyboardViewController : IMKeyboardViewController
+
+@end
---
+++ Examples/Keyboard/Source/View Controllers/IMSampleKeyboardViewController.m
@@ -0,0 +1,21 @@
+//
+//  IMSampleKeyboardViewController.m
+//  imoji-keyboard
+//
+//  Created by Nima on 8/31/15.
+//  Copyright © 2015 Jeff. All rights reserved.
+//
+
+#import "IMSampleKeyboardViewController.h"
+
+@implementation IMSampleKeyboardViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
