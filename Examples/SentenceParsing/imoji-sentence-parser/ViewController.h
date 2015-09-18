4d21b910dcf77448444d8822dda7402261d1cd06
nkhoshini <nima@imojiapp.com>
2015-09-18 15:43:32 -0700

    sentence parsing sample project

---
+++ Examples/SentenceParsing/imoji-sentence-parser/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  imoji-sentence-parser
+//
+//  Created by Nima on 9/18/15.
+//  Copyright © 2015 Imoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ImojiSDKUI/IMCollectionViewController.h>
+
+@interface ViewController : IMCollectionViewController
+
+
+@end
+
---
+++ Examples/SentenceParsing/imoji-sentence-parser/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  imoji-sentence-parser
+//
+//  Created by Nima on 9/18/15.
+//  Copyright © 2015 Imoji. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "IMCollectionView.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)loadView {
+    [super loadView];
+    self.sentenceParseEnabled = YES;
+}
+
+@end
