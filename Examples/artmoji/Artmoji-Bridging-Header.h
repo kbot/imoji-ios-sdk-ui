23683e11fb166402632017eb2c418990ff968dbb
Alex Hoang <alex.hoang.dev@gmail.com>
2015-09-24 17:47:26 -0700

    Add bridging header file.

---
+++ Examples/artmoji/Artmoji-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Artmoji-Bridging-Header.h
+//  artmoji
+//
+//  Created by Alex Hoang on 9/24/15.
+//  Copyright (c) 2015 Imoji. All rights reserved.
+//
+
+#ifndef artmoji_Artmoji_Bridging_Header_h
+#define artmoji_Artmoji_Bridging_Header_h
+
+#import "ImojiSDK/ImojiSDK.h"
+
+#endif
