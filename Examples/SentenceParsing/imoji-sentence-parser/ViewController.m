ffb5176bd0b75ff84d6c1d58e0cdede9bf17c0d0
nkhoshini <nima@imojiapp.com>
2015-09-24 18:56:35 -0700

    update sentence parser example

--- Examples/SentenceParsing/imoji-sentence-parser/ViewController.m
+++ Examples/SentenceParsing/imoji-sentence-parser/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "IMCollectionView.h"
 
-@interface ViewController ()
+@interface ViewController () <IMCollectionViewControllerDelegate>
 
 @end
 
@@ -18,6 +18,33 @@
 - (void)loadView {
     [super loadView];
     self.sentenceParseEnabled = YES;
+    self.collectionViewControllerDelegate = self;
 }
+
+- (void)userDidSelectImoji:(IMImojiObject *__nonnull)imoji fromCollectionView:(IMCollectionView *__nonnull)collectionView {
+    IMImojiObjectRenderingOptions *renderingOptions =
+            [IMImojiObjectRenderingOptions optionsWithRenderSize:IMImojiObjectRenderSizeFullResolution];
+    renderingOptions.aspectRatio = [NSValue valueWithCGSize:CGSizeMake(16.0f, 9.0f)];
+
+    [self.session renderImoji:imoji
+                      options:renderingOptions
+                     callback:^(UIImage *image, NSError *error) {
+                         NSArray *sharingItems = @[image];
+                         UIActivityViewController *activityController = [[UIActivityViewController alloc] initWithActivityItems:sharingItems
+                                                                                                          applicationActivities:nil];
+                         activityController.excludedActivityTypes = @[
+                                 UIActivityTypePrint,
+                                 UIActivityTypeCopyToPasteboard,
+                                 UIActivityTypeAssignToContact,
+                                 UIActivityTypeSaveToCameraRoll,
+                                 UIActivityTypeAddToReadingList,
+                                 UIActivityTypePostToFlickr,
+                                 UIActivityTypePostToVimeo
+                         ];
+
+                         [self presentViewController:activityController animated:YES completion:nil];
+                     }];
+}
+
 
 @end
